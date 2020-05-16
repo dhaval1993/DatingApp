0bb1005e76243ea858460820f54faa8671fb00a8
Dhaval Gopujkar <dhaval.gopujkar@tieto.com>
2020-05-16 23:28:42 +0530

    end of section 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './Value/Value.component';
-
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule} from '@angular/forms';
 @NgModule({
    declarations: [
       AppComponent,
-     
-      ValueComponent
+      ValueComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
